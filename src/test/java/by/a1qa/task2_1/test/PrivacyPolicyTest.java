7babb4902821e2130e34eec79294fd1c595da29d
‘dmitriystel’ <dmitriystel@gmail.com>
2022-12-21 09:00:02 +0300

    refactoring

---
+++ src/test/java/by/a1qa/task2_1/test/PrivacyPolicyTest.java
@@ -0,0 +1,31 @@
+package by.a1qa.task2_1.test;
+
+import by.a1qa.task2_1.page.MainPage;
+import by.a1qa.task2_1.page.GameSearchResultPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import by.a1qa.task2_1.page.PrivacyPolicyPage;
+
+import java.io.IOException;
+import java.time.Year;
+
+public class PrivacyPolicyTest extends BaseTest {
+    @Test
+    public void testPrivacyPolicySignedInCurrentYear() throws IOException {
+        MainPage mainPage = new MainPage(driver);
+        PrivacyPolicyPage privacyPolicyPage;
+
+        privacyPolicyPage = mainPage
+                .navigateToMainPage()
+                .scrollAndOpenPrivacyPolicy();
+
+        Assert.assertTrue(privacyPolicyPage.isSwitchLanguageElementsListDisplayed(),
+                "Privacy policy page is not open in the new tab.");
+
+        String policyRevisionString = privacyPolicyPage
+                .getPolicyRevisionString();
+
+        Assert.assertTrue(policyRevisionString.contains(Integer.toString(Year.now().getValue())),
+                "Policy revision didn't sign in the current year.");
+    }
+}
